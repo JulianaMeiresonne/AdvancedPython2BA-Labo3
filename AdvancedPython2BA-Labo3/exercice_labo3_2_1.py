6c61fcf293009aa85bdab594851667c09357e119
Juliana <23342ecam@gmail.com>
2025-03-26 19:01:26 +0100

    init commit

--- AdvancedPython2BA-Labo3/exercice_labo3_2_1.py
+++ AdvancedPython2BA-Labo3/exercice_labo3_2_1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ''''
 Définir une fonction call qui reçoit en paramètre une fonction sans paramètres qui ne renvoie pas de valeur et qui l’appelle. 
 On doit, par exemple, pouvoir écrire le code suivant :
@@ -15,7 +14,3 @@ def call(fct):
     return fct
 
 call(hello())
-=======
-'''
-'''
->>>>>>> init commit
